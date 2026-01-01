ec5a8f7946fbd57f859c3a59b3451c012f374b69
Jowohny <vujohny109238@gmail.com>
2025-12-31 22:38:31 -0800

    error page
    just something in the case that i add upcoming component parts that are incomplete

---
+++ app/error.vue
@@ -0,0 +1,9 @@
+<template>
+	<NuxtLayout>
+		<div class="min-h-[80vh] flex items-center justify-center">
+			<h1 class="font-semibold w-2/3 text-8xl text-center text-transparent bg-clip-text bg-[url('/static.jpg')]">
+				Unfortunately, this component hasn't been developed quite yet, come back soon...
+			</h1>
+		</div>
+	</NuxtLayout>
+</template>
