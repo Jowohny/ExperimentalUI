90eebeeda6b7c1751bb38b9139b765b65d774c6d
Jowohny <vujohny109238@gmail.com>
2026-01-13 23:50:15 -0800

    implemented examples for detail card
    added icons

--- app/pages/CardExamples.vue
+++ app/pages/CardExamples.vue
@@ -47,72 +47,114 @@
 		}
 	]
 
-const linkItem = [
-	{
-		logo: "/ButtonLogos/youtubelogo.png",
-		id: "39284",
-		title: "YouTube",
-		link: "https://youtube.com",
-		color: "text-amber-600"
-	},
-	{
-		logo: "/ButtonLogos/googlelogo.png",
-		id: "21345",
-		title: "Google",
-		link: "https://google.com",
-		color: "text-blue-600"
-	},
-	{
-		logo: "/ButtonLogos/netflixlogo.png",
-		id: "25321",
-		title: "Netflix",
-		link: "https://netflix.com",
-		color: "text-orange-600"
-	},
-	{
-		logo: "/ButtonLogos/facebooklogo.png",
-		id: "25234",
-		title: "Facebook",
-		link: "https://facebook.com",
-		color: "text-indigo-600"
-	},
-	{
-		logo: "/ButtonLogos/amazonlogo.png",
-		id: "74543",
-		title: "Amazon",
-		link: "https://amazon.com",
-		color: "text-amber-600"
-	},
-	{
-		logo: "/ButtonLogos/githublogo.png",
-		id: "83735",
-		title: "Github",
-		link: "https://github.com",
-		color: "text-blue-600"
-	},
-	{
-		id: "84635",
-		title: "Johny's Dev Portfolio",
-		link: "https://vujohny.com",
-		color: "text-orange-600"
-	},
-	{
-		logo: "/ButtonLogos/linkedinlogo.png",
-		id: "18347",
-		title: "LinkedIn",
-		link: "https://linkedin.com",
-		color: "text-indigo-600"
-	}
-]
+	const linkItem = [
+		{
+			logo: "/ButtonLogos/youtubelogo.png",
+			id: "39284",
+			title: "YouTube",
+			link: "https://youtube.com",
+			color: "text-amber-600"
+		},
+		{
+			logo: "/ButtonLogos/googlelogo.png",
+			id: "21345",
+			title: "Google",
+			link: "https://google.com",
+			color: "text-blue-600"
+		},
+		{
+			logo: "/ButtonLogos/netflixlogo.png",
+			id: "25321",
+			title: "Netflix",
+			link: "https://netflix.com",
+			color: "text-orange-600"
+		},
+		{
+			logo: "/ButtonLogos/facebooklogo.png",
+			id: "25234",
+			title: "Facebook",
+			link: "https://facebook.com",
+			color: "text-indigo-600"
+		},
+		{
+			logo: "/ButtonLogos/amazonlogo.png",
+			id: "74543",
+			title: "Amazon",
+			link: "https://amazon.com",
+			color: "text-amber-600"
+		},
+		{
+			logo: "/ButtonLogos/githublogo.png",
+			id: "83735",
+			title: "Github",
+			link: "https://github.com",
+			color: "text-blue-600"
+		},
+		{
+			id: "84635",
+			title: "Johny's Dev Portfolio",
+			link: "https://vujohny.com",
+			color: "text-orange-600"
+		},
+		{
+			logo: "/ButtonLogos/linkedinlogo.png",
+			id: "18347",
+			title: "LinkedIn",
+			link: "https://linkedin.com",
+			color: "text-indigo-600"
+		}
+	]
 
+	const techStack = [
+		{
+			techType: 'Framework',
+			id: 'vue',
+			name: 'Vue.js',
+			description: 'The Progressive JavaScript Framework used for building user interfaces.',
+			icon: '/TechStackIcons/NuxtIcon.webp',
+			colSpan: 'col-span-3',
+			color: 'text-green-400'
+		},
+		{
+			techType: 'Library', 
+			id: 'gsap',
+			name: 'GSAP',
+			description: 'Professional-grade animation library for the modern web.',
+			icon: '/TechStackIcons/GSAPIcon.png',
+			colSpan: 'col-span-2',
+			color: 'text-emerald-500'
+		},
+		{
+			techType: 'Language',
+			id: 'swift',
+			name: 'Swift',
+			description: 'High-level general-purpose, multi-paradigm, compiled programming language, maintained by the open-source community.',
+			icon: '/TechStackIcons/SwiftIcon.png',
+			colSpan: 'col-span-3',
+			color: 'text-orange-600'
+		},
+		{
+			techType: 'Framework', 
+			id: 'swiftui',
+			name: 'SwiftUI',
+			description: 'An innovative, exceptionally simple way to build user interfaces across all Apple platforms with the power of Swift.',
+			icon: '/TechStackIcons/SwiftUIIcon.png',
+			colSpan: 'col-span-4',
+			color: 'text-blue-400'
+		}
+	]
 </script>
 
 <template>
-	<div class="grid grid-cols-5 gap-4 px-4 pb-12">
+	<div class="grid grid-cols-5 gap-4 px-4 mb-12">
 		<CardTypeBase :class="card.colSpan" v-for="card in bentoItems" :item="card" />
 	</div>
 
-	<div class="flex flex-wrap gap-4 px-4">
+	<div class="flex flex-wrap gap-4 px-4 mb-12">
 		<CardTypeLink class="w-fit" v-for="card in linkItem" :item="card"/>
 	</div>
+
+	<div class="grid grid-cols-8 gap-4 p-8">
+     <CardTypeDetailDisplay v-for="tech in techStack" :class="tech.colSpan" :item="tech" />
+  </div>
 </template>
